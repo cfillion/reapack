f5a6aae2a56eca8ea7707a944b016dec0c7c5840
cfillion <cfillion@users.noreply.github.com>
2015-12-16 17:50:39 -0800

    setup dual build for both platforms

---
+++ wrapper.bat
@@ -0,0 +1,23 @@
+@echo off
+
+set arch=%1%
+set program=%2%
+set args=
+
+:next_arg
+if "%3%"=="" goto continue
+set args=%args% %3%
+shift
+goto next_arg
+:continue
+
+if "%arch%"=="x64" (
+  call "%VCINSTALLDIR%\vcvarsall.bat" x86_amd64
+) else ( if "%arch%"=="x86" (
+  call "%VCINSTALLDIR%\vcvarsall.bat" x86
+) else (
+  echo Unknown Architecture: %arch%
+  exit /b 42
+))
+
+"%program%" %args%
