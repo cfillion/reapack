bfcf384cb776336be442be1f5e91c0b097707565
cfillion <cfillion@users.noreply.github.com>
2015-12-30 01:04:42 -0500

    oops, fix 521e943e8616d20a6c79d1d61665527d47748fd7 on windows

--- src/menu.cpp
+++ src/menu.cpp
@@ -17,12 +17,6 @@
 
 #include "menu.hpp"
 
-#ifdef _WIN32
-#define MENUITEMINFO MENUITEMINFOA
-#undef InsertMenuItem
-#define InsertMenuItem InsertMenuItemA
-#endif
-
 Menu::Menu(HMENU handle)
   : m_handle(handle)
 {
