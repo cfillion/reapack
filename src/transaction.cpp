cfillion/reapack

commit f34a0552189b03eeee3369526d653231ee9ac9fa

refactor package queries in the registry

src/transaction.cpp
@@ -133,14 +133,16 @@ void Transaction::saveIndex(Download *dl, const string &name)
 void Transaction::upgrade(const Package *pkg)
 {
   const Version *ver = pkg->lastVersion();
-  auto queryRes = m_registry->query(pkg);
+  const Registry::Entry &regEntry = m_registry->getEntry(pkg);
 
-  if(queryRes.status == Registry::UpToDate) {
+  InstallTicket::Type type = InstallTicket::Install;
+
+  if(regEntry.id && regEntry.version == ver->code()) {
     if(allFilesExists(ver->files()))
       return; // latest version is really installed, nothing to do here!
-    else
-      queryRes.status = Registry::Uninstalled;
   }
+  else if(regEntry.version < ver->code())
+    type = InstallTicket::Upgrade;
 
   // prevent file conflicts – pushes to the registry will be reverted!
   try {
@@ -164,7 +166,7 @@ void Transaction::upgrade(const Package *pkg)
   }
 
   // all green! (pronounce with a japanese accent)
-  m_installQueue.push({ver, queryRes});
+  m_installQueue.push({type, ver, regEntry});
 }
 
 void Transaction::installQueued()
@@ -180,7 +182,7 @@ void Transaction::installQueued()
 void Transaction::installTicket(const InstallTicket &ticket)
 {
   const Version *ver = ticket.version;
-  const set<Path> &currentFiles = m_registry->getFiles(ticket.regQuery.entry);
+  const set<Path> &currentFiles = m_registry->getFiles(ticket.regEntry);
 
   InstallTask *task = new InstallTask(ver, currentFiles, this);
 

src/receipt.cpp
@@ -26,10 +26,14 @@ Receipt::Receipt()
 
 void Receipt::addTicket(const InstallTicket &ticket)
 {
-  if(ticket.regQuery.status == Registry::UpdateAvailable)
+  switch(ticket.type) {
+  case InstallTicket::Upgrade:
     m_updates.push_back(ticket);
-  else
+    break;
+  default:
     m_installs.push_back(ticket);
+    break;
+  }
 }
 
 void Receipt::addRemovals(const std::set<Path> &pathList)

src/registry.cpp
@@ -184,19 +184,6 @@ auto Registry::push(const Version *ver, vector<Path> *conflicts) -> Entry
   }
 }
 
-auto Registry::query(const Package *pkg) const -> QueryResult
-{
-  const Entry &entry = getEntry(pkg);
-
-  if(!entry.id)
-    return {};
-
-  const Status status =
-    entry.version == pkg->lastVersion()->code() ? UpToDate : UpdateAvailable;
-
-  return {status, entry};
-}
-
 auto Registry::getEntry(const Package *pkg) const -> Entry
 {
   int id = 0;

src/registry.hpp
@@ -32,14 +32,6 @@ class Version;
 
 class Registry {
 public:
-  Registry(const Path &path = Path());
-
-  enum Status {
-    Uninstalled,
-    UpdateAvailable,
-    UpToDate,
-  };
-
   struct Entry {
     int id;
     std::string remote;
@@ -49,12 +41,8 @@ public:
     Version::Code version;
   };
 
-  struct QueryResult {
-    Status status;
-    Entry entry;
-  };
+  Registry(const Path &path = Path());
 
-  QueryResult query(const Package *) const;
   Entry getEntry(const Package *) const;
   std::vector<Entry> getEntries(const std::string &) const;
   std::set<Path> getFiles(const Entry &) const;

src/report.cpp
@@ -134,13 +134,13 @@ void Report::printUpdates()
 
   for(const InstallTicket &ticket : m_receipt->updates()) {
     const Package *pkg = ticket.version->package();
-    const auto &queryRes = ticket.regQuery;
+    const Registry::Entry &regEntry = ticket.regEntry;
     const VersionSet &versions = pkg->versions();
 
     stream() << pkg->fullName() << ':' << NL;
 
     for(const Version *ver : versions | boost::adaptors::reversed) {
-      if(ver->code() <= queryRes.entry.version)
+      if(ver->code() <= regEntry.version)
         break;
 
       printVersion(ver);

src/transaction.hpp
@@ -32,8 +32,11 @@ class RemoteIndex;
 class Task;
 
 struct InstallTicket {
+  enum Type { Install, Upgrade };
+
+  Type type;
   const Version *version;
-  const Registry::QueryResult regQuery;
+  const Registry::Entry regEntry;
 };
 
 struct HostTicket { bool add; Registry::Entry entry; std::string file; };

test/registry.cpp
@@ -27,13 +27,12 @@ TEST_CASE("query uninstalled package", M) {
 
   Registry reg;
 
-  const auto &res = reg.query(&pkg);
-  REQUIRE(res.status == Registry::Uninstalled);
-  REQUIRE(res.entry.id == 0);
-  REQUIRE(res.entry.version == 0);
+  const Registry::Entry &res = reg.getEntry(&pkg);
+  REQUIRE(res.id == 0);
+  REQUIRE(res.version == 0);
 }
 
-TEST_CASE("query up to date pacakge", M) {
+TEST_CASE("query installed pacakge", M) {
   MAKE_PACKAGE
 
   Registry reg;
@@ -46,9 +45,13 @@ TEST_CASE("query up to date pacakge", M) {
   REQUIRE(entry.type == Package::ScriptType);
   REQUIRE(entry.version == Version("1.0").code());
 
-  const Registry::QueryResult &queryRes = reg.query(&pkg);
-  REQUIRE(queryRes.status == Registry::UpToDate);
-  REQUIRE(queryRes.entry.version == Version("1.0").code());
+  const Registry::Entry &selectEntry = reg.getEntry(&pkg);
+  REQUIRE(selectEntry.id == entry.id);
+  REQUIRE(selectEntry.remote == entry.remote);
+  REQUIRE(selectEntry.category == entry.category);
+  REQUIRE(selectEntry.package == entry.package);
+  REQUIRE(selectEntry.type == entry.type);
+  REQUIRE(selectEntry.version == entry.version);
 }
 
 TEST_CASE("bump version", M) {
@@ -61,27 +64,25 @@ TEST_CASE("bump version", M) {
   reg.push(ver);
   pkg.addVersion(ver2);
 
-  const Registry::QueryResult &res1 = reg.query(&pkg);
-  REQUIRE(res1.status == Registry::UpdateAvailable);
-  REQUIRE(res1.entry.version == Version("1.0").code());
+  const Registry::Entry &entry1 = reg.getEntry(&pkg);
+  REQUIRE(entry1.version == Version("1.0").code());
 
   reg.push(ver2);
-  const Registry::QueryResult &res2 = reg.query(&pkg);
-  REQUIRE(res2.status == Registry::UpToDate);
-  REQUIRE(res2.entry.version == Version("2.0").code());
+  const Registry::Entry &entry2 = reg.getEntry(&pkg);
+  REQUIRE(entry2.version == Version("2.0").code());
   
-  REQUIRE(res2.entry.id == res1.entry.id);
+  REQUIRE(entry2.id == entry1.id);
 }
 
 TEST_CASE("get file list", M) {
   MAKE_PACKAGE
 
   Registry reg;
-  REQUIRE(reg.getFiles(reg.query(&pkg).entry).empty());
+  REQUIRE(reg.getFiles(reg.getEntry(&pkg)).empty());
 
   reg.push(ver);
 
-  const set<Path> &files = reg.getFiles(reg.query(&pkg).entry);
+  const set<Path> &files = reg.getFiles(reg.getEntry(&pkg));
 
   REQUIRE(files == ver->files());
 }
@@ -112,9 +113,8 @@ TEST_CASE("forget registry entry", M) {
   Registry reg;
   reg.forget(reg.push(ver));
 
-  const Registry::QueryResult &afterForget = reg.query(&pkg);
-  REQUIRE(afterForget.status == Registry::Uninstalled);
-  REQUIRE(afterForget.entry.id == 0);
+  const Registry::Entry &afterForget = reg.getEntry(&pkg);
+  REQUIRE(afterForget.id == 0); // uninstalled
 }
 
 TEST_CASE("file conflicts", M) {
@@ -135,7 +135,7 @@ TEST_CASE("file conflicts", M) {
   ver->addSource(src2);
   pkg.addVersion(ver);
 
-  CHECK(reg.query(&pkg).status == Registry::Uninstalled);
+  CHECK(reg.getEntry(&pkg).id == 0); // uninstalled
 
   try {
     reg.push(ver);
@@ -143,7 +143,7 @@ TEST_CASE("file conflicts", M) {
   }
   catch(const reapack_error &) {}
 
-  REQUIRE(reg.query(&pkg).status == Registry::Uninstalled);
+  CHECK(reg.getEntry(&pkg).id == 0); // still uninstalled
 
   vector<Path> conflicts;
   reg.push(ver, &conflicts);
@@ -151,7 +151,7 @@ TEST_CASE("file conflicts", M) {
   REQUIRE(conflicts.size() == 1);
   REQUIRE(conflicts[0] == src1->targetPath());
 
-  REQUIRE(reg.query(&pkg).status == Registry::Uninstalled);
+  REQUIRE(reg.getEntry(&pkg).id == 0); // never installed
 }
 
 TEST_CASE("get main file", M) {

commit b129dc9cbcc526ded65536ab523d781e2887fa43

fix the labelling of new installations, was broken since f34a0552189b03eeee3369526d653231ee9ac9fa

src/transaction.cpp
@@ -133,12 +133,14 @@ void Transaction::upgrade(const Package *pkg)
 
   InstallTicket::Type type = InstallTicket::Install;
 
-  if(regEntry.id && regEntry.version == ver->code()) {
-    if(allFilesExists(ver->files()))
-      return; // latest version is really installed, nothing to do here!
+  if(regEntry.id) {
+    if(regEntry.version == ver->code()) {
+      if(allFilesExists(ver->files()))
+        return; // latest version is really installed, nothing to do here!
+    }
+    else if(regEntry.version < ver->code())
+      type = InstallTicket::Upgrade;
   }
-  else if(regEntry.version < ver->code())
-    type = InstallTicket::Upgrade;
 
   // prevent file conflicts – pushes to the registry will be reverted!
   try {