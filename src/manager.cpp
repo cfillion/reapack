2ebb322f4b521e2829fc915591fce5ecba13a7cc
cfillion <cfillion@users.noreply.github.com>
2016-05-05 01:05:31 -0400

    manager: rename URL column to "Index URL"

--- src/manager.cpp
+++ src/manager.cpp
@@ -47,7 +47,7 @@ void Manager::onInit()
 
   m_list = createControl<ListView>(IDC_LIST, ListView::Columns{
     {AUTO_STR("Name"), 110},
-    {AUTO_STR("URL"), 350},
+    {AUTO_STR("Index URL"), 350},
     {AUTO_STR("State"), 60},
   });
 
