6f20328d8ae900102f036d6408bf026a95972201
cfillion <cfillion@users.noreply.github.com>
2016-06-25 21:31:04 -0700

    commit missing changes for xp support

--- wrapper.bat
+++ wrapper.bat
@@ -21,4 +21,19 @@ if "%arch%"=="x86_64" (
   exit /b 42
 ))
 
+:: Windows XP support
+:: https://blogs.msdn.microsoft.com/vcblog/2012/10/08/windows-xp-targeting-with-c-in-visual-studio-2012/
+:: http://www.tripleboot.org/?p=423
+set PF86=%ProgramFiles(x86)%
+set INCLUDE=%PF86%\Microsoft SDKs\Windows\v7.1A\Include;%INCLUDE%
+set PATH=%PF86%\Microsoft SDKs\Windows\v7.1A\Bin;%PATH%
+set LIB=%PF86%\Microsoft SDKs\Windows\v7.1A\Lib;%LIB%
+set CL=/D_WIN32_WINNT#0x0501 /D_USING_V110_SDK71_ /D_ATL_XP_TARGETING %CL%
+if "%arch%"=="x86_64" (
+  set LIB=!PF86!\Microsoft SDKs\Windows\v7.1A\Lib\x64;!LIB!
+  set LINK=/SUBSYSTEM:CONSOLE,5.02 %LINK%
+) else (
+  set LINK=/SUBSYSTEM:CONSOLE,5.01 %LINK%
+)
+
 "%program%" %args%
