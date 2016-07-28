4508622f793e82e064e39c6b072db97b80762f60
cfillion <cfillion@users.noreply.github.com>
2016-07-27 20:35:51 -0400

    browser: better default layout than in 378e87cd2d58ac7b52990502f0707d3cd9c737dd.

--- src/browser.cpp
+++ src/browser.cpp
@@ -80,10 +80,10 @@ void Browser::onInit()
 
   m_list = createControl<ListView>(IDC_LIST, ListView::Columns{
     {AUTO_STR("Status"), 23, ListView::NoLabelFlag},
-    {AUTO_STR("Package"), 370},
-    {AUTO_STR("Category"), 150},
-    {AUTO_STR("Version"), 80},
-    {AUTO_STR("Author"), 120},
+    {AUTO_STR("Package"), 310},
+    {AUTO_STR("Category"), 100},
+    {AUTO_STR("Version"), 70},
+    {AUTO_STR("Author"), 105},
     {AUTO_STR("Type"), 70},
     {AUTO_STR("Repository"), 120, ListView::CollapseFlag},
     {AUTO_STR("Last Update"), 120},
