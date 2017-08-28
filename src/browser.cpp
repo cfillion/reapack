cfillion/reapack

commit e39b7f20e6466c3719425b00a941b5121e3eba75

browser: manager: re-sort list after changing row contents

src/browser.cpp
@@ -1012,8 +1012,10 @@ void Browser::updateAction(const int index)
     m_visibleEntries.erase(m_visibleEntries.begin() + index);
     updateDisplayLabel();
   }
-  else
+  else {
     m_list->replaceRow(index, makeRow(*entry));
+    m_list->sort(); // TODO: only re-sort if sorted by status column
+  }
 
   if(m_actions.empty())
     disable(m_applyBtn);

src/manager.cpp
@@ -364,6 +364,9 @@ void Manager::setMods(const ModsCallback &cb, const bool updateRow)
     if(updateRow)
       m_list->replaceRow(index, makeRow(remote));
   }
+
+  if(updateRow)
+    m_list->sort();
 }
 
 void Manager::setRemoteEnabled(const bool enabled)
@@ -396,7 +399,7 @@ void Manager::setRemoteAutoInstall(const tribool &enabled)
       mods->autoInstall = boost::none;
     else
       mods->autoInstall = enabled;
-  }, true);
+  }, false);
 }
 
 tribool Manager::remoteAutoInstall(const Remote &remote) const

src/manager.hpp
@@ -59,7 +59,7 @@ private:
 
   Remote getRemote(int index) const;
   bool fillContextMenu(Menu &, int index) const;
-  void setMods(const ModsCallback &, bool updateRow = false);
+  void setMods(const ModsCallback &, bool updateRow);
   void setRemoteEnabled(bool);
   bool isRemoteEnabled(const Remote &) const;
   void setRemoteAutoInstall(const tribool &);

commit 2188cc6523d0cbef4e69a62d427ff466a8db2302

browser: fix performance degradation caused by e39b7f20e6466c3719425b00a941b5121e3eba75
Doing actions on the entire package list got very slow.

src/browser.cpp
@@ -752,12 +752,8 @@ void Browser::updateAction(const int index)
     m_list->removeRow(index);
     updateDisplayLabel();
   }
-  else {
+  else
     m_list->row(index)->setCell(0, make_autostring(entry->displayState()));
-
-    if(m_list->sortColumn() == 0)
-      m_list->sort();
-  }
 
   if(m_actions.empty())
     disable(m_applyBtn);
@@ -781,6 +777,11 @@ void Browser::selectionDo(const function<void (int)> &func)
       offset++;
     lastSize = newSize;
   }
+
+  // re-sort here rather than doing it once per entry after updating the
+  // status cell in updateAction
+  if(m_list->sortColumn() == 0)
+    m_list->sort();
 }
 
 auto Browser::currentView() const -> View