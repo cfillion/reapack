cfillion/reapack

commit 4599355b119629eb87587aab8394acead3154dd0

manager: make the about repository dialog follow selection

src/manager.cpp
@@ -72,7 +72,8 @@ void Manager::onInit()
     {AUTO_STR("State"), 60},
   });
 
-  m_list->onActivate([=] { m_reapack->about(getRemote(m_list->currentIndex())); });
+  m_list->onActivate(bind(&Manager::aboutRepo, this, true));
+  m_list->onSelect(bind(&Dialog::startTimer, this, 100, 0, true));
   m_list->onContextMenu(bind(&Manager::fillContextMenu,
     this, placeholders::_1, placeholders::_2));
 
@@ -91,6 +92,16 @@ void Manager::onInit()
   refresh();
 
   m_list->autoSizeHeader();
+}
+
+void Manager::onTimer(const int id)
+{
+  stopTimer(id);
+
+  if(About *about = m_reapack->about(false)) {
+    if(about->testDelegate<AboutIndexDelegate>())
+      aboutRepo(false);
+  }
 }
 
 void Manager::onClose()
@@ -461,6 +472,11 @@ void Manager::copyUrl()
     values.push_back(getRemote(index).url());
 
   setClipboard(values);
+}
+
+void Manager::aboutRepo(const bool focus)
+{
+  m_reapack->about(getRemote(m_list->currentIndex()), focus);
 }
 
 void Manager::importExport()

src/manager.hpp
@@ -45,6 +45,7 @@ protected:
   void onInit() override;
   void onCommand(int, int) override;
   bool onKeyDown(int, int) override;
+  void onTimer(int) override;
   void onClose() override;
 
 private:
@@ -75,6 +76,7 @@ private:
   void setupNetwork();
   void importArchive();
   void exportArchive();
+  void aboutRepo(bool focus = true);
 
   void setChange(int);
   bool confirm() const;

src/reapack.cpp
@@ -227,7 +227,7 @@ Remote ReaPack::remote(const string &name) const
   return m_config->remotes.get(name);
 }
 
-void ReaPack::about(const Remote &repo)
+void ReaPack::about(const Remote &repo, const bool focus)
 {
   Transaction *tx = setupTransaction();
   if(!tx)
@@ -239,7 +239,7 @@ void ReaPack::about(const Remote &repo)
   tx->onFinish([=] {
     const auto &indexes = tx->getIndexes(repos);
     if(!indexes.empty())
-      about()->setDelegate(make_shared<AboutIndexDelegate>(indexes.front()));
+      about()->setDelegate(make_shared<AboutIndexDelegate>(indexes.front()), focus);
   });
   tx->runTasks();
 }

src/reapack.hpp
@@ -69,7 +69,7 @@ public:
   void importRemote();
   void manageRemotes();
   void aboutSelf();
-  void about(const Remote &);
+  void about(const Remote &, bool focus = true);
   About *about(bool instantiate = true);
   Browser *browsePackages();
   void refreshManager();

commit 8783b9746ede48dc2709ab2ddbb212eb1a22eab5

manager: don't attempt to load empty repository into about dialog (broken since 4599355b119629eb87587aab8394acead3154dd0)

src/manager.cpp
@@ -486,7 +486,8 @@ void Manager::copyUrl()
 
 void Manager::aboutRepo(const bool focus)
 {
-  g_reapack->about(getRemote(m_list->currentIndex()), focus);
+  if(m_list->hasSelection())
+    g_reapack->about(getRemote(m_list->currentIndex()), focus);
 }
 
 void Manager::importExport()