cfillion/reapack

commit 76ba324156336015937bf3a27397766c20eac191

abstract away most of the UTF-8 to UTF-16 conversion for Windows

src/dialog.cpp
@@ -18,7 +18,6 @@
 #include "dialog.hpp"
 
 #include "control.hpp"
-#include "encoding.hpp"
 
 #include <algorithm>
 #include <boost/range/adaptor/map.hpp>
@@ -331,11 +330,11 @@ void Dialog::stopTimer(int id)
 
 void Dialog::setClipboard(const string &text)
 {
-  const auto_string &data = make_autostring(text);
-  const size_t length = (data.size() + 1) * sizeof(auto_char); // null terminator
+  // calculate the size in bytes including the null terminator
+  const size_t size = (text.size() + 1) * sizeof(char);
 
-  HANDLE mem = GlobalAlloc(GMEM_MOVEABLE, length);
-  memcpy(GlobalLock(mem), data.c_str(), length);
+  HANDLE mem = GlobalAlloc(GMEM_MOVEABLE, size);
+  memcpy(GlobalLock(mem), text.c_str(), size);
   GlobalUnlock(mem);
 
   OpenClipboard(m_handle);
@@ -354,26 +353,9 @@ void Dialog::setClipboard(const vector<string> &values)
     setClipboard(boost::algorithm::join(values, "\n"));
 }
 
-void Dialog::openURL(const string &utf8)
-{
-  const auto_string &url = make_autostring(utf8);
-  ShellExecute(nullptr, AUTO_STR("open"), url.c_str(), nullptr, nullptr, SW_SHOW);
-}
-
 HWND Dialog::getControl(const int idc)
 {
   return GetDlgItem(m_handle, idc);
-}
-
-string Dialog::getText(HWND handle)
-{
-  auto_string buffer(4096, 0);
-  GetWindowText(handle, &buffer[0], (int)buffer.size());
-
-  // remove extra nulls from the string
-  buffer.resize(buffer.find(AUTO_STR('\0')));
-
-  return from_autostring(buffer);
 }
 
 void Dialog::setAnchor(HWND handle, const int flags)

src/win32.cpp
@@ -1,0 +1,99 @@
+/* ReaPack: Package manager for REAPER
+ * Copyright (C) 2015-2017  Christian Fillion
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "win32.hpp"
+
+#ifdef _WIN32
+#  define widen_cstr(cstr) widen(cstr).c_str()
+#else
+#  include <swell/swell.h>
+#  define widen_cstr(cstr) cstr
+#endif
+
+using namespace std;
+
+#ifdef _WIN32
+wstring Win32::widen(const char *input, const UINT codepage)
+{
+  const int size = MultiByteToWideChar(codepage, 0, input, -1, nullptr, 0) - 1;
+
+  wstring output(size, 0);
+  MultiByteToWideChar(codepage, 0, input, -1, &output[0], size);
+
+  return output;
+}
+
+string Win32::narrow(const wchar_t *input)
+{
+  const int size = WideCharToMultiByte(CP_UTF8, 0,
+    input, -1, nullptr, 0, nullptr, nullptr) - 1;
+
+  string output(size, 0);
+  WideCharToMultiByte(CP_UTF8, 0, input, -1, &output[0], size, nullptr, nullptr);
+
+  return output;
+}
+#endif
+
+int Win32::messageBox(const HWND handle, const char *text,
+  const char *title, const unsigned int buttons)
+{
+  return MessageBox(handle, widen_cstr(text), widen_cstr(title), buttons);
+}
+
+string Win32::getWindowText(const HWND handle)
+{
+  char_type buffer[4096];
+  GetWindowText(handle, buffer, (int)lengthof(buffer));
+
+  return narrow(buffer);
+}
+
+void Win32::setWindowText(const HWND handle, const char *text)
+{
+  SetWindowText(handle, widen_cstr(text));
+}
+
+void Win32::shellExecute(const char *what, const char *arg)
+{
+  ShellExecute(nullptr, L("open"), widen_cstr(what), widen_cstr(arg), nullptr, SW_SHOW);
+}
+
+bool Win32::writePrivateProfileString(const char *group, const char *key,
+  const char *value, const char *path)
+{
+  // value can be null for deleting a key
+  return WritePrivateProfileString(widen_cstr(group), widen_cstr(key),
+    value ? widen_cstr(value) : nullptr, widen_cstr(path));
+}
+
+string Win32::getPrivateProfileString(const char *group, const char *key,
+  const char *fallback, const char *path)
+{
+  char_type buffer[4096];
+  GetPrivateProfileString(widen_cstr(group), widen_cstr(key),
+    widen_cstr(fallback), buffer, lengthof(buffer), widen_cstr(path));
+
+  return narrow(buffer);
+}
+
+unsigned int Win32::getPrivateProfileInt(const char *group, const char *key,
+  const unsigned int fallback, const char *path)
+{
+  return GetPrivateProfileInt(widen_cstr(group), widen_cstr(key),
+    fallback, widen_cstr(path));
+}

src/win32.hpp
@@ -1,0 +1,64 @@
+/* ReaPack: Package manager for REAPER
+ * Copyright (C) 2015-2017  Christian Fillion
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef REAPACK_WIN32_HPP
+#define REAPACK_WIN32_HPP
+
+// Utility wrappers around the Windows Wide API
+
+#include <string>
+
+#ifdef _WIN32
+#  define L(str) L##str
+#  include <windows.h>
+#else
+#  define L(str) str
+#  include <swell-types.h>
+#endif
+
+#define lengthof(t) (sizeof(t) / sizeof(*t))
+
+namespace Win32 {
+#ifdef _WIN32
+  typedef wchar_t char_type;
+
+  std::wstring widen(const char *, UINT codepage = CP_UTF8);
+  inline std::wstring widen(const std::string &str) { return widen(str.c_str(), CP_UTF8); }
+
+  std::string narrow(const wchar_t *);
+  inline std::string narrow(const std::wstring &str) { return narrow(str.c_str()); }
+#else
+  typedef char char_type;
+
+  inline std::string widen(const char *s, UINT = 0) { return s; }
+  inline std::string widen(const std::string &s) { return s; }
+
+  inline std::string narrow(const char *s) { return s; }
+  inline std::string narrow(const std::string &s) { return s; }
+#endif
+
+  int messageBox(HWND, const char *text, const char *title, unsigned int buttons);
+  void setWindowText(HWND handle, const char *text);
+  std::string getWindowText(HWND handle);
+  void shellExecute(const char *what, const char *arg = nullptr);
+
+  bool writePrivateProfileString(const char *g, const char *k, const char *v, const char *p);
+  std::string getPrivateProfileString(const char *g, const char *k, const char *v, const char *p);
+  unsigned int getPrivateProfileInt(const char *g, const char *k, unsigned int f, const char *p);
+};
+
+#endif

test/win32.cpp
@@ -1,0 +1,37 @@
+#include "helper.hpp"
+
+#include <win32.hpp>
+
+using namespace std;
+
+static const char *M = "[win32]";
+
+TEST_CASE("widen string", M) {
+  SECTION("ascii") {
+    const auto &wide = Win32::widen("hello world");
+    REQUIRE(wide == L("hello world"));
+    REQUIRE(wide.size() == 11);
+  }
+
+  SECTION("cyrillic") {
+    const auto &wide = Win32::widen("世界");
+    REQUIRE(wide == L("\u4e16\u754c"));
+#ifdef _WIN32
+    REQUIRE(wide.size() == 2);
+#else
+    REQUIRE(wide.size() == 6);
+#endif
+  }
+}
+
+TEST_CASE("narrow string", M) {
+  SECTION("ascii") {
+    const auto &narrow = Win32::narrow(L("hello world"));
+    REQUIRE(narrow == "hello world");
+  }
+
+  SECTION("cyrillic") {
+    const auto &narrow = Win32::narrow(L("\u4e16\u754c"));
+    REQUIRE(narrow == "世界");
+  }
+}

src/about.cpp
@@ -19,7 +19,6 @@
 
 #include "browser.hpp"
 #include "config.hpp"
-#include "encoding.hpp"
 #include "errors.hpp"
 #include "filesystem.hpp"
 #include "index.hpp"
@@ -34,10 +33,10 @@
 #include "richedit.hpp"
 #include "tabbar.hpp"
 #include "transaction.hpp"
+#include "win32.hpp"
 
 #include <boost/algorithm/string.hpp>
 #include <iomanip>
-#include <sstream>
 
 using namespace std;
 
@@ -171,15 +170,14 @@ void About::setDelegate(const DelegatePtr &delegate, const bool focus)
 
 void About::setTitle(const string &what)
 {
-  auto_char title[255];
-  auto_snprintf(title, auto_size(title),
-    AUTO_STR("About %s"), make_autostring(what).c_str());
-  SetWindowText(handle(), title);
+  char title[255];
+  snprintf(title, sizeof(title), "About %s", what.c_str());
+  Win32::setWindowText(handle(), title);
 }
 
 void About::setMetadata(const Metadata *metadata, const bool substitution)
 {
-  string aboutText = metadata->about();
+  string aboutText(metadata->about());
 
   if(substitution) {
     boost::replace_all(aboutText, "[[REAPACK_VERSION]]", ReaPack::VERSION);
@@ -187,7 +185,7 @@ void About::setMetadata(const Metadata *metadata, const bool substitution)
   }
 
   if(m_desc->setRichText(aboutText))
-    m_tabs->addTab({AUTO_STR("About"), {m_desc->handle()}});
+    m_tabs->addTab({"About", {m_desc->handle()}});
 
   const auto &getLinkControl = [](const Metadata::LinkType type) {
     switch(type) {
@@ -232,7 +230,7 @@ void About::setMetadata(const Metadata *metadata, const bool substitution)
 void About::setAction(const string &label)
 {
   HWND btn = getControl(IDC_ACTION);
-  SetWindowText(btn, make_autostring(label).c_str());
+  Win32::setWindowText(btn, label.c_str());
   show(btn);
 }
 
@@ -244,7 +242,7 @@ void About::selectLink(const int ctrl)
   m_tabs->setFocus();
 
   if(count == 1) {
-    openURL(links.front()->url);
+    Win32::shellExecute(links.front()->url.c_str());
     return;
   }
 
@@ -252,13 +250,13 @@ void About::selectLink(const int ctrl)
 
   for(int i = 0; i < count; i++) {
     const string &name = boost::replace_all_copy(links[i]->name, "&", "&&");
-    menu.addAction(make_autostring(name).c_str(), i | (ctrl << 8));
+    menu.addAction(name.c_str(), i | (ctrl << 8));
   }
 
   const int choice = menu.show(getControl(ctrl), handle());
 
   if(choice >> 8 == ctrl)
-    openURL(links[choice & 0xff]->url);
+    Win32::shellExecute(links[choice & 0xff]->url.c_str());
 }
 
 void About::updateList()
@@ -291,29 +289,29 @@ void AboutIndexDelegate::init(About *dialog)
   dialog->setMetadata(m_index->metadata(), m_index->name() == "ReaPack");
   dialog->setAction("Install/update " + m_index->name());
 
-  dialog->tabs()->addTab({AUTO_STR("Packages"),
+  dialog->tabs()->addTab({"Packages",
     {dialog->menu()->handle(), dialog->list()->handle()}});
-  dialog->tabs()->addTab({AUTO_STR("Installed Files"),
+  dialog->tabs()->addTab({"Installed Files",
     {dialog->getControl(IDC_REPORT)}});
 
-  dialog->menu()->addColumn({AUTO_STR("Category"), 142});
+  dialog->menu()->addColumn({"Category", 142});
 
   dialog->menu()->reserveRows(m_index->categories().size() + 1);
-  dialog->menu()->createRow()->setCell(0, AUTO_STR("<All Packages>"));
+  dialog->menu()->createRow()->setCell(0, "<All Packages>");
 
   for(const Category *cat : m_index->categories())
-    dialog->menu()->createRow()->setCell(0, make_autostring(cat->name()));
+    dialog->menu()->createRow()->setCell(0, cat->name());
 
-  dialog->list()->addColumn({AUTO_STR("Package"), 382});
-  dialog->list()->addColumn({AUTO_STR("Version"), 80, 0, ListView::VersionType});
-  dialog->list()->addColumn({AUTO_STR("Author"), 90});
+  dialog->list()->addColumn({"Package", 382});
+  dialog->list()->addColumn({"Version", 80, 0, ListView::VersionType});
+  dialog->list()->addColumn({"Author", 90});
 
   initInstalledFiles();
 }
 
 void AboutIndexDelegate::initInstalledFiles()
 {
-  HWND report = m_dialog->getControl(IDC_REPORT);
+  const HWND report = m_dialog->getControl(IDC_REPORT);
 
   set<Registry::File> allFiles;
 
@@ -325,24 +323,21 @@ void AboutIndexDelegate::initInstalledFiles()
     }
   }
   catch(const reapack_error &e) {
-    const auto_string &desc = make_autostring(e.what());
-    auto_char msg[255];
-    auto_snprintf(msg, auto_size(msg),
-      AUTO_STR("The file list is currently unavailable.\x20")
-      AUTO_STR("Retry later when all installation task are completed.\r\n")
-      AUTO_STR("\r\nError description: %s"),
-      desc.c_str());
-    SetWindowText(report, msg);
+    char msg[255];
+    snprintf(msg, sizeof(msg),
+      "The file list is currently unavailable.\x20"
+      "Retry later when all installation task are completed.\r\n"
+      "\r\nError description: %s", e.what());
+    Win32::setWindowText(report, msg);
     return;
   }
 
   if(allFiles.empty()) {
-    SetWindowText(report,
-      AUTO_STR(
-      "This repository does not own any file on your computer at this time.\r\n")
+    Win32::setWindowText(report,
+      "This repository does not own any file on your computer at this time.\r\n"
 
-      AUTO_STR("It is either not yet installed or it does not provide ")
-      AUTO_STR("any package compatible with your system."));
+      "It is either not yet installed or it does not provide "
+      "any package compatible with your system.");
   }
   else {
     stringstream stream;
@@ -354,7 +349,7 @@ void AboutIndexDelegate::initInstalledFiles()
       stream << "\r\n";
     }
 
-    SetWindowText(report, make_autostring(stream.str()).c_str());
+    Win32::setWindowText(report, stream.str().c_str());
   }
 }
 
@@ -377,9 +372,9 @@ void AboutIndexDelegate::updateList(const int index)
     const Version *lastVer = pkg->lastVersion();
 
     auto row = m_dialog->list()->createRow((void *)pkg);
-    row->setCell(c++, make_autostring(pkg->displayName()));
-    row->setCell(c++, make_autostring(lastVer->name().toString()), (void *)&lastVer->name());
-    row->setCell(c++, make_autostring(lastVer->displayAuthor()));
+    row->setCell(c++, pkg->displayName());
+    row->setCell(c++, lastVer->name().toString(), (void *)&lastVer->name());
+    row->setCell(c++, lastVer->displayAuthor());
   }
 }
 
@@ -388,8 +383,8 @@ bool AboutIndexDelegate::fillContextMenu(Menu &menu, const int index) const
   if(index < 0)
     return false;
 
-  menu.addAction(AUTO_STR("Find in the &browser"), ACTION_FIND_IN_BROWSER);
-  menu.addAction(AUTO_STR("About this &package"), ACTION_ABOUT_PKG);
+  menu.addAction("Find in the &browser", ACTION_FIND_IN_BROWSER);
+  menu.addAction("About this &package", ACTION_ABOUT_PKG);
 
   return true;
 }
@@ -458,8 +453,8 @@ void AboutIndexDelegate::install()
   enum { INSTALL_ALL = 80, UPDATE_ONLY };
 
   Menu menu;
-  menu.addAction(AUTO_STR("Install all packages in this repository"), INSTALL_ALL);
-  menu.addAction(AUTO_STR("Update installed packages only"), UPDATE_ONLY);
+  menu.addAction("Install all packages in this repository", INSTALL_ALL);
+  menu.addAction("Update installed packages only", UPDATE_ONLY);
 
   const int choice = menu.show(m_dialog->getControl(IDC_ACTION), m_dialog->handle());
 
@@ -470,9 +465,9 @@ void AboutIndexDelegate::install()
 
   if(!remote) {
     // In case the user uninstalled the repository while this dialog was opened
-    MessageBox(m_dialog->handle(),
-      AUTO_STR("This repository cannot be found in your current configuration."),
-      AUTO_STR("ReaPack"), MB_OK);
+    Win32::messageBox(m_dialog->handle(),
+      "This repository cannot be found in your current configuration.",
+      "ReaPack", MB_OK);
     return;
   }
 
@@ -480,12 +475,12 @@ void AboutIndexDelegate::install()
 
   if(choice == INSTALL_ALL && boost::logic::indeterminate(remote.autoInstall())
       && !installOpts.autoInstall) {
-    const int btn = MessageBox(m_dialog->handle(),
-      AUTO_STR("Do you want ReaPack to install new packages from this repository")
-      AUTO_STR(" when synchronizing in the future?\r\n\r\nThis setting can also be")
-      AUTO_STR(" customized globally or on a per-repository basis in")
-      AUTO_STR(" ReaPack > Manage repositories."),
-      AUTO_STR("Install all packages in this repository"), MB_YESNOCANCEL);
+    const int btn = Win32::messageBox(m_dialog->handle(),
+      "Do you want ReaPack to install new packages from this repository"
+      " when synchronizing in the future?\r\n\r\nThis setting can also be"
+      " customized globally or on a per-repository basis in"
+      " ReaPack > Manage repositories.",
+      "Install all packages in this repository", MB_YESNOCANCEL);
 
     switch(btn) {
     case IDYES:
@@ -523,21 +518,21 @@ void AboutPackageDelegate::init(About *dialog)
   dialog->setMetadata(m_package->metadata());
   dialog->setAction("About " + m_index->name());
 
-  dialog->tabs()->addTab({AUTO_STR("History"),
+  dialog->tabs()->addTab({"History",
     {dialog->menu()->handle(), dialog->getControl(IDC_CHANGELOG)}});
-  dialog->tabs()->addTab({AUTO_STR("Contents"),
+  dialog->tabs()->addTab({"Contents",
     {dialog->menu()->handle(), dialog->list()->handle()}});
 
-  dialog->menu()->addColumn({AUTO_STR("Version"), 142, 0, ListView::VersionType});
+  dialog->menu()->addColumn({"Version", 142, 0, ListView::VersionType});
 
-  dialog->list()->addColumn({AUTO_STR("File"), 474});
-  dialog->list()->addColumn({AUTO_STR("Action List"), 84});
+  dialog->list()->addColumn({"File", 474});
+  dialog->list()->addColumn({"Action List", 84});
 
   dialog->menu()->reserveRows(m_package->versions().size());
 
   for(const Version *ver : m_package->versions()) {
     auto row = dialog->menu()->createRow();
-    row->setCell(0, make_autostring(ver->name().toString()), (void *)&ver->name());
+    row->setCell(0, ver->name().toString(), (void *)&ver->name());
 
     if(m_current == ver->name())
       dialog->menu()->select(row->index());
@@ -563,10 +558,10 @@ void AboutPackageDelegate::updateList(const int index)
   const Version *ver = m_package->version(index);
   OutputStream stream;
   stream << *ver;
-  SetWindowText(m_dialog->getControl(IDC_CHANGELOG),
-    make_autostring(stream.str()).c_str());
+  Win32::setWindowText(m_dialog->getControl(IDC_CHANGELOG), stream.str().c_str());
 
   m_dialog->list()->reserveRows(ver->sources().size());
+
   for(const Source *src : ver->sources()) {
     int sections = src->sections();
     string actionList;
@@ -593,8 +588,8 @@ void AboutPackageDelegate::updateList(const int index)
 
     int c = 0;
     auto row = m_dialog->list()->createRow((void *)src);
-    row->setCell(c++, make_autostring(src->targetPath().join()));
-    row->setCell(c++, make_autostring(actionList));
+    row->setCell(c++, src->targetPath().join());
+    row->setCell(c++, actionList);
   }
 }
 
@@ -605,9 +600,9 @@ bool AboutPackageDelegate::fillContextMenu(Menu &menu, const int index) const
 
   auto src = (const Source *)m_dialog->list()->row(index)->userData;
 
-  menu.addAction(AUTO_STR("Copy source URL"), ACTION_COPY_URL);
+  menu.addAction("Copy source URL", ACTION_COPY_URL);
   menu.setEnabled(m_current.size() > 0 && FS::exists(src->targetPath()),
-    menu.addAction(AUTO_STR("Locate in explorer/finder"), ACTION_LOCATE));
+    menu.addAction("Locate in explorer/finder", ACTION_LOCATE));
 
   return true;
 }
@@ -651,11 +646,10 @@ void AboutPackageDelegate::locate()
     if(!FS::exists(path))
       return;
 
-    string arg = "/select,\"";
+    string arg("/select,\"");
     arg += Path::prefixRoot(path).join();
     arg += '"';
 
-    ShellExecute(nullptr, AUTO_STR("open"), AUTO_STR("explorer.exe"),
-      make_autostring(arg).c_str(), nullptr, SW_SHOW);
+    Win32::shellExecute("explorer.exe", arg.c_str());
   }
 }

src/archive.cpp
@@ -24,6 +24,7 @@
 #include "path.hpp"
 #include "reapack.hpp"
 #include "transaction.hpp"
+#include "win32.hpp"
 
 #include <boost/format.hpp>
 #include <fstream>
@@ -34,10 +35,10 @@
 #include <zlib/unzip.h>
 #include <zlib/ioapi.h>
 
-using namespace boost;
+using boost::format;
 using namespace std;
 
-static const Path ARCHIVE_TOC = Path("toc");
+static const Path ARCHIVE_TOC("toc");
 static const size_t BUFFER_SIZE = 4096;
 
 #ifdef _WIN32
@@ -54,8 +55,10 @@ static void *wide_fopen(voidpf, const void *filename, int mode)
 
   FILE *file = nullptr;
 
-  if(filename && fopen_mode)
-    _wfopen_s(&file, static_cast<const wchar_t *>(filename), fopen_mode);
+  if(filename && fopen_mode) {
+    const auto &&wideFilename = Win32::widen(static_cast<const char *>(filename));
+    _wfopen_s(&file, wideFilename.c_str(), fopen_mode);
+  }
 
   return file;
 }
@@ -71,7 +74,7 @@ struct ImportArchive {
   IndexPtr m_lastIndex;
 };
 
-void Archive::import(const auto_string &path)
+void Archive::import(const string &path)
 {
   ImportArchive state{make_shared<ArchiveReader>(path),
     &g_reapack->config()->remotes};
@@ -105,7 +108,7 @@ void Archive::import(const auto_string &path)
       }
     }
     catch(const reapack_error &e) {
-      state.m_tx->receipt()->addError({e.what(), from_autostring(path)});
+      state.m_tx->receipt()->addError({e.what(), path});
     }
   }
 
@@ -152,15 +155,15 @@ void ImportArchive::importPackage(const string &data)
   const Version *ver = pkg ? pkg->findVersion(versionName) : nullptr;
 
   if(!ver) {
-    throw reapack_error(format("%s/%s/%s v%s cannot be found or is"
-      " incompatible with your operating system.")
+    throw reapack_error(format(
+      "%s/%s/%s v%s cannot be found or is incompatible with your operating system.")
       % m_lastIndex->name() % categoryName % packageName % versionName);
   }
 
   m_tx->install(ver, pinned, m_reader);
 }
 
-ArchiveReader::ArchiveReader(const auto_string &path)
+ArchiveReader::ArchiveReader(const string &path)
 {
   zlib_filefunc64_def filefunc;
   fill_fopen64_filefunc(&filefunc);
@@ -168,10 +171,10 @@ ArchiveReader::ArchiveReader(const auto_string &path)
   filefunc.zopen64_file = wide_fopen;
 #endif
 
-  m_zip = unzOpen2_64(reinterpret_cast<const char *>(path.c_str()), &filefunc);
+  m_zip = unzOpen2_64(path.c_str(), &filefunc);
 
   if(!m_zip)
-    throw reapack_error(FS::lastError().c_str());
+    throw reapack_error(FS::lastError());
 }
 
 ArchiveReader::~ArchiveReader()
@@ -241,7 +244,7 @@ bool FileExtractor::run()
   return true;
 }
 
-size_t Archive::create(const auto_string &path, vector<string> *errors,
+size_t Archive::create(const string &path, vector<string> *errors,
   ThreadPool *pool)
 {
   size_t count = 0;
@@ -296,7 +299,7 @@ size_t Archive::create(const auto_string &path, vector<string> *errors,
   return count;
 }
 
-ArchiveWriter::ArchiveWriter(const auto_string &path)
+ArchiveWriter::ArchiveWriter(const string &path)
 {
   zlib_filefunc64_def filefunc;
   fill_fopen64_filefunc(&filefunc);
@@ -304,11 +307,10 @@ ArchiveWriter::ArchiveWriter(const auto_string &path)
   filefunc.zopen64_file = wide_fopen;
 #endif
 
-  m_zip = zipOpen2_64(reinterpret_cast<const char *>(path.c_str()),
-    APPEND_STATUS_CREATE, nullptr, &filefunc);
+  m_zip = zipOpen2_64(path.c_str(), APPEND_STATUS_CREATE, nullptr, &filefunc);
 
   if(!m_zip)
-    throw reapack_error(FS::lastError().c_str());
+    throw reapack_error(FS::lastError());
 }
 
 ArchiveWriter::~ArchiveWriter()

src/archive.hpp
@@ -18,7 +18,6 @@
 #ifndef REAPACK_ARCHIVE_HPP
 #define REAPACK_ARCHIVE_HPP
 
-#include "encoding.hpp"
 #include "path.hpp"
 #include "thread.hpp"
 
@@ -27,14 +26,13 @@ class ThreadPool;
 typedef void *zipFile;
 
 namespace Archive {
-  void import(const auto_string &path);
-  size_t create(const auto_string &path,
-    std::vector<std::string> *errors, ThreadPool *pool);
+  void import(const std::string &path);
+  size_t create(const std::string &path, std::vector<std::string> *errors, ThreadPool *pool);
 };
 
 class ArchiveReader {
 public:
-  ArchiveReader(const auto_string &path);
+  ArchiveReader(const std::string &path);
   ~ArchiveReader();
   int extractFile(const Path &);
   int extractFile(const Path &, std::ostream &) noexcept;
@@ -47,7 +45,7 @@ typedef std::shared_ptr<ArchiveReader> ArchiveReaderPtr;
 
 class ArchiveWriter {
 public:
-  ArchiveWriter(const auto_string &path);
+  ArchiveWriter(const std::string &path);
   ~ArchiveWriter();
   int addFile(const Path &fn);
   int addFile(const Path &fn, std::istream &) noexcept;

commit 827cffa45b156de60415d724395258a5293ecaca

dialog: fix clipboard copy on Windows spewing garbage (broken since 76ba324156336015937bf3a27397766c20eac191)

src/dialog.cpp
@@ -18,6 +18,7 @@
 #include "dialog.hpp"
 
 #include "control.hpp"
+#include "win32.hpp"
 
 #include <algorithm>
 #include <boost/range/adaptor/map.hpp>
@@ -330,12 +331,7 @@ void Dialog::stopTimer(int id)
 
 void Dialog::setClipboard(const string &text)
 {
-  // calculate the size in bytes including the null terminator
-  const size_t size = (text.size() + 1) * sizeof(char);
-
-  HANDLE mem = GlobalAlloc(GMEM_MOVEABLE, size);
-  memcpy(GlobalLock(mem), text.c_str(), size);
-  GlobalUnlock(mem);
+  const HANDLE mem = Win32::globalCopy(text);
 
   OpenClipboard(m_handle);
   EmptyClipboard();

src/win32.cpp
@@ -73,6 +73,18 @@ void Win32::shellExecute(const char *what, const char *arg)
   ShellExecute(nullptr, L("open"), widen_cstr(what), widen_cstr(arg), nullptr, SW_SHOW);
 }
 
+HANDLE Win32::globalCopy(const string &text)
+{
+  // calculate the size in bytes including the null terminator
+  const size_t size = (text.size() + 1) * sizeof(char_type);
+
+  HANDLE mem = GlobalAlloc(GMEM_MOVEABLE, size);
+  memcpy(GlobalLock(mem), widen_cstr(text.c_str()), size);
+  GlobalUnlock(mem);
+
+  return mem;
+}
+
 bool Win32::writePrivateProfileString(const char *group, const char *key,
   const char *value, const char *path)
 {

src/win32.hpp
@@ -55,6 +55,7 @@ namespace Win32 {
   void setWindowText(HWND handle, const char *text);
   std::string getWindowText(HWND handle);
   void shellExecute(const char *what, const char *arg = nullptr);
+  HANDLE globalCopy(const std::string &);
 
   bool writePrivateProfileString(const char *g, const char *k, const char *v, const char *p);
   std::string getPrivateProfileString(const char *g, const char *k, const char *v, const char *p);