cfillion/reapack

commit 0f6a51caa78d437eeeecf279fee285b2e49c090e

receipt: fix missing newline when updating through >1 versions [#21]

src/receipt.cpp
@@ -120,7 +120,7 @@ ostream &operator<<(ostream &os, const InstallTicket &t)
   if(os.tellp() > 0)
     os << "\r\n";
 
-  os << t.m_version->package()->fullName() << "\r\n";
+  os << t.m_version->package()->fullName();
 
   if(t.m_isUpdate) {
     const auto &versions = t.m_version->package()->versions();
@@ -129,7 +129,7 @@ ostream &operator<<(ostream &os, const InstallTicket &t)
       if(ver->name() <= t.m_previous)
         break;
       else if(ver->name() <= t.m_version->name())
-        os << *ver;
+        os << "\r\n" << *ver;
     }
   }
   else

test/receipt.cpp
@@ -5,6 +5,7 @@
 #include <index.hpp>
 
 using Catch::Matchers::Contains;
+using Catch::Matchers::EndsWith;
 
 using namespace std;
 
@@ -174,7 +175,8 @@ TEST_CASE("format install ticket", M) {
     entry.version = VersionName("1.0");
     stream << InstallTicket{v3, entry};
     REQUIRE_THAT(stream.str(),
-      !Contains("v1.0") && Contains("v2.0") && Contains("v3.0"));
+      !Contains("v1.0") && Contains("\r\nv3.0\r\n  No changelog\r\nv2.0"));
+    REQUIRE_THAT(stream.str(), !EndsWith("\r\n"));
   }
 
   SECTION("downgrade") {

commit db756818fd4c09e2813cf8ba062378ba304de736

oops, add missing newline when installing a new package (regression from 0f6a51caa78d437eeeecf279fee285b2e49c090e)

src/receipt.cpp
@@ -133,7 +133,7 @@ ostream &operator<<(ostream &os, const InstallTicket &t)
     }
   }
   else
-    os << *t.m_version;
+    os << "\r\n" << *t.m_version;
 
   return os;
 }