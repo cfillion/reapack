cfillion/reapack

commit 98030aabd6b93a20d0fcffc56b7137d5aaff023c

progress: don't open while a modal dialog is shown [p=1920318]
Otherwise the progress dialog might be displayed above on Windows.

src/progress.cpp
@@ -58,6 +58,9 @@ void Progress::onCommand(const int id, int)
 
 void Progress::onTimer(const int id)
 {
+  if(!IsWindowEnabled(handle()))
+    return;
+
   show();
   stopTimer(id);
 }

commit 4b22d928e60de898a31241082abfc6ec0796bcbb

woops, fix progress dialog on macOS (broken in 98030aa)

src/progress.cpp
@@ -58,8 +58,10 @@ void Progress::onCommand(const int id, int)
 
 void Progress::onTimer(const int id)
 {
+#ifdef _WIN32
   if(!IsWindowEnabled(handle()))
     return;
+#endif
 
   show();
   stopTimer(id);