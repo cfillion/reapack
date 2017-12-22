26bd658d07ae64bbf2524dd53652c1e21d525c1e
cfillion <cfillion@users.noreply.github.com>
2017-12-22 14:24:33 -0500

    (woops, fix bad indentation)

--- src/archive_tasks.cpp
+++ src/archive_tasks.cpp
@@ -82,8 +82,8 @@ bool ExportTask::start()
   // thread at the same time.
   for(FileCompressor *job : jobs) {
     job->onFinish([=] {
-    if(job->state() == ThreadTask::Success)
-      tx()->receipt()->addExport(job->path());
+      if(job->state() == ThreadTask::Success)
+        tx()->receipt()->addExport(job->path());
     });
 
     tx()->threadPool()->push(job);
