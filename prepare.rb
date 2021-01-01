fe9c9da16639e4406a99dfb852e81bd08a134bda
cfillion <cfillion@users.noreply.github.com>
2021-01-01 00:00:11 -0500

    happy new year 2021! :fireworks:

--- prepare.rb
+++ prepare.rb
@@ -28,7 +28,7 @@ end
 
 __END__
 /* ReaPack: Package manager for REAPER
- * Copyright (C) 2015-2020  Christian Fillion
+ * Copyright (C) 2015-2021  Christian Fillion
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
