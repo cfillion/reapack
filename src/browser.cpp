eada515538eb5f0a63f31c15c04b6dfa56934f46
cfillion <cfillion@users.noreply.github.com>
2022-01-06 01:50:12 -0500

    browser: fix typing parentheses in the filter box

--- src/browser.cpp
+++ src/browser.cpp
@@ -212,7 +212,7 @@ void Browser::onCommand(const int id, const int event)
 bool Browser::onKeyDown(const int key, const int mods)
 {
   if(GetFocus() != m_list->handle()) {
-    if(key == VK_UP || key == VK_DOWN)
+    if(mods == 0 && (key == VK_UP || key == VK_DOWN))
       SetFocus(m_list->handle());
 
     return false;
