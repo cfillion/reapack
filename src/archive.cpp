cfillion/reapack

commit ecdc99394a260ea6fe802af996d6e9fd38a63168

split download and thread logic

src/download.cpp
@@ -20,6 +20,7 @@
 #include "reapack.hpp"
 
 #include <boost/format.hpp>
+#include <curl/curl.h>
 
 #include <reaper_plugin_functions.h>
 
@@ -28,12 +29,10 @@ using namespace std;
 
 static const int DOWNLOAD_TIMEOUT = 15;
 // to set the amount of concurrent downloads, change the size of
-// the m_pool member in DownloadQueue
+// the m_pool member in ThreadPool (thread.hpp)
 
 static CURLSH *g_curlShare = nullptr;
 static WDL_Mutex g_curlMutex;
-
-DownloadNotifier *DownloadNotifier::s_instance = nullptr;
 
 static void LockCurlMutex(CURL *, curl_lock_data, curl_lock_access, void *)
 {
@@ -45,7 +44,7 @@ static void UnlockCurlMutex(CURL *, curl_lock_data, curl_lock_access, void *)
   g_curlMutex.Leave();
 }
 
-void Download::Init()
+void Download::GlobalInit()
 {
   curl_global_init(CURL_GLOBAL_DEFAULT);
 
@@ -59,10 +58,36 @@ void Download::Init()
   curl_share_setopt(g_curlShare, CURLSHOPT_SHARE, CURL_LOCK_DATA_SSL_SESSION);
 }
 
-void Download::Cleanup()
+void Download::GlobalCleanup()
 {
   curl_share_cleanup(g_curlShare);
   curl_global_cleanup();
+}
+
+void *Download::ContextInit()
+{
+  CURL *curl = curl_easy_init();
+
+  const auto userAgent = format("ReaPack/%s REAPER/%s")
+    % ReaPack::VERSION % GetAppVersion();
+
+  curl_easy_setopt(curl, CURLOPT_USERAGENT, userAgent.str().c_str());
+  curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 1);
+  curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, DOWNLOAD_TIMEOUT);
+  curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, DOWNLOAD_TIMEOUT);
+  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, true);
+  curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 5);
+  curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "");
+  curl_easy_setopt(curl, CURLOPT_FAILONERROR, true);
+  curl_easy_setopt(curl, CURLOPT_SHARE, g_curlShare);
+  curl_easy_setopt(curl, CURLOPT_NOPROGRESS, false);
+
+  return curl;
+}
+
+void Download::ContextCleanup(void *ctx)
+{
+  curl_easy_cleanup(static_cast<CURL *>(ctx));
 }
 
 size_t Download::WriteData(char *data, size_t rawsize, size_t nmemb, void *ptr)
@@ -82,49 +107,28 @@ int Download::UpdateProgress(void *ptr, const double, const double,
 
 Download::Download(const string &name, const string &url,
   const NetworkOpts &opts, const int flags)
-  : m_name(name), m_url(url), m_opts(opts), m_flags(flags),
-    m_state(Idle), m_abort(false)
+  : m_name(name), m_url(url), m_opts(opts), m_flags(flags)
 {
-  DownloadNotifier::get()->start();
 }
 
-Download::~Download()
+string Download::summary() const
 {
-  DownloadNotifier::get()->stop();
-}
-
-void Download::setState(const State state)
-{
-  m_state = state;
-
-  switch(state) {
-  case Idle:
-    break;
-  case Running:
-    m_onStart();
-    break;
-  case Success:
-  case Failure:
-  case Aborted:
-    m_onFinish();
-    m_cleanupHandler();
-    break;
-  }
+  return "Downloading %s: " + m_name;
 }
 
 void Download::start()
 {
-  DownloadThread *thread = new DownloadThread;
+  WorkerThread *thread = new WorkerThread;
   thread->push(this);
   onFinish([thread] { delete thread; });
 }
 
-void Download::exec(CURL *curl)
+void Download::run(CURL *curl)
 {
   const auto finish = [&](const State state, const string &contents) {
     m_contents = contents;
 
-    DownloadNotifier::get()->notify({this, state});
+    ThreadNotifier::get()->notify({this, state});
   };
 
   if(m_abort) {
@@ -132,7 +136,7 @@ void Download::exec(CURL *curl)
     return;
   }
 
-  DownloadNotifier::get()->notify({this, Running});
+  ThreadNotifier::get()->notify({this, Running});
 
   string contents;
 
@@ -166,164 +170,4 @@ void Download::exec(CURL *curl)
     finish(Success, contents);
 
   curl_slist_free_all(headers);
-}
-
-DownloadThread::DownloadThread() : m_exit(false)
-{
-  m_wake = CreateEvent(nullptr, true, false, AUTO_STR("WakeEvent"));
-  m_thread = CreateThread(nullptr, 0, thread, (void *)this, 0, nullptr);
-}
-
-DownloadThread::~DownloadThread()
-{
-  // remove all pending downloads then wake the thread to make it exit
-  m_exit = true;
-  SetEvent(m_wake);
-
-  WaitForSingleObject(m_thread, INFINITE);
-
-  CloseHandle(m_wake);
-  CloseHandle(m_thread);
-}
-
-DWORD WINAPI DownloadThread::thread(void *ptr)
-{
-  DownloadThread *thread = static_cast<DownloadThread *>(ptr);
-  CURL *curl = curl_easy_init();
-
-  const auto userAgent = format("ReaPack/%s REAPER/%s")
-    % ReaPack::VERSION % GetAppVersion();
-
-  curl_easy_setopt(curl, CURLOPT_USERAGENT, userAgent.str().c_str());
-  curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 1);
-  curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, DOWNLOAD_TIMEOUT);
-  curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, DOWNLOAD_TIMEOUT);
-  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, true);
-  curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 5);
-  curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "");
-  curl_easy_setopt(curl, CURLOPT_FAILONERROR, true);
-  curl_easy_setopt(curl, CURLOPT_SHARE, g_curlShare);
-  curl_easy_setopt(curl, CURLOPT_NOPROGRESS, false);
-
-  while(!thread->m_exit) {
-    while(Download *dl = thread->nextDownload())
-      dl->exec(curl);
-
-    ResetEvent(thread->m_wake);
-    WaitForSingleObject(thread->m_wake, INFINITE);
-  }
-
-  curl_easy_cleanup(curl);
-
-  return 0;
-}
-
-Download *DownloadThread::nextDownload()
-{
-  WDL_MutexLock lock(&m_mutex);
-
-  if(m_queue.empty())
-    return nullptr;
-
-  Download *dl = m_queue.front();
-  m_queue.pop();
-  return dl;
-}
-
-void DownloadThread::push(Download *dl)
-{
-  WDL_MutexLock lock(&m_mutex);
-
-  m_queue.push(dl);
-  SetEvent(m_wake);
-}
-
-DownloadQueue::~DownloadQueue()
-{
-  // don't emit DownloadQueue::onAbort from the destructor
-  // which is most likely to cause a crash
-  m_onAbort.disconnect_all_slots();
-
-  abort();
-}
-
-void DownloadQueue::push(Download *dl)
-{
-  m_onPush(dl);
-  m_running.insert(dl);
-
-  dl->onFinish([=] {
-    m_running.erase(dl);
-
-    // call m_onDone() only after every onFinish slots ran
-    if(m_running.empty())
-      m_onDone();
-  });
-
-  dl->setCleanupHandler([=] { delete dl; });
-
-  auto &thread = m_pool[m_running.size() % m_pool.size()];
-  if(!thread)
-    thread = make_unique<DownloadThread>();
-
-  thread->push(dl);
-}
-
-void DownloadQueue::abort()
-{
-  for(Download *dl : m_running)
-    dl->abort();
-
-  m_onAbort();
-}
-
-DownloadNotifier *DownloadNotifier::get()
-{
-  if(!s_instance)
-    s_instance = new DownloadNotifier;
-
-  return s_instance;
-}
-
-void DownloadNotifier::start()
-{
-  if(!m_active++)
-    plugin_register("timer", (void *)tick);
-}
-
-void DownloadNotifier::stop()
-{
-  --m_active;
-}
-
-void DownloadNotifier::notify(const Notification &notif)
-{
-  WDL_MutexLock lock(&m_mutex);
-
-  m_queue.push(notif);
-}
-
-void DownloadNotifier::tick()
-{
-  DownloadNotifier *instance = DownloadNotifier::get();
-  instance->processQueue();
-
-  // doing this in stop() would cause a use after free of m_mutex in processQueue
-  if(!instance->m_active) {
-    plugin_register("-timer", (void *)tick);
-
-    delete s_instance;
-    s_instance = nullptr;
-  }
-}
-
-void DownloadNotifier::processQueue()
-{
-  WDL_MutexLock lock(&m_mutex);
-
-  while(!m_queue.empty()) {
-    const Notification &notif = m_queue.front();
-    notif.first->setState(notif.second);
-    m_queue.pop();
-  }
 }

src/progress.cpp
@@ -17,17 +17,17 @@
 
 #include "progress.hpp"
 
-#include "download.hpp"
+#include "thread.hpp"
 #include "resource.hpp"
 
 using namespace std;
 
-Progress::Progress(DownloadQueue *queue)
+Progress::Progress(ThreadPool *pool)
   : Dialog(IDD_PROGRESS_DIALOG),
-    m_queue(queue), m_label(nullptr), m_progress(nullptr),
+    m_pool(pool), m_label(nullptr), m_progress(nullptr),
     m_done(0), m_total(0)
 {
-  m_queue->onPush(bind(&Progress::addDownload, this, placeholders::_1));
+  m_pool->onPush(bind(&Progress::addTask, this, placeholders::_1));
 }
 
 void Progress::onInit()
@@ -44,7 +44,7 @@ void Progress::onCommand(const int id, int)
 {
   switch(id) {
   case IDCANCEL:
-    m_queue->abort();
+    m_pool->abort();
 
     // don't wait until the current downloads are finished
     // before getting out of the user way
@@ -59,7 +59,7 @@ void Progress::onTimer(const int id)
   stopTimer(id);
 }
 
-void Progress::addDownload(Download *dl)
+void Progress::addTask(ThreadTask *task)
 {
   m_total++;
   updateProgress();
@@ -67,12 +67,12 @@ void Progress::addDownload(Download *dl)
   if(!isVisible())
     startTimer(100);
 
-  dl->onStart([=] {
-    m_currentName = make_autostring(dl->name());
+  task->onStart([=] {
+    m_current = make_autostring(task->summary());
     updateProgress();
   });
 
-  dl->onFinish([=] {
+  task->onFinish([=] {
     m_done++;
     updateProgress();
   });
@@ -80,9 +80,12 @@ void Progress::addDownload(Download *dl)
 
 void Progress::updateProgress()
 {
+  auto_char position[32];
+  auto_snprintf(position, auto_size(position), AUTO_STR("%d of %d"),
+    min(m_done + 1, m_total), m_total);
+
   auto_char label[1024];
-  auto_snprintf(label, auto_size(label), AUTO_STR("Downloading %d of %d: %s"),
-    min(m_done + 1, m_total), m_total, m_currentName.c_str());
+  auto_snprintf(label, auto_size(label), m_current.c_str(), position);
 
   SetWindowText(m_label, label);
 
@@ -91,7 +94,7 @@ void Progress::updateProgress()
 
   auto_char title[255];
   auto_snprintf(title, auto_size(title),
-    AUTO_STR("ReaPack: Download in progress (%d%%)"), percent);
+    AUTO_STR("ReaPack: Operation in progress (%d%%)"), percent);
 
   SendMessage(m_progress, PBM_SETPOS, percent, 0);
   SetWindowText(handle(), title);

src/progress.hpp
@@ -22,12 +22,12 @@
 
 #include "encoding.hpp"
 
-class Download;
-class DownloadQueue;
+class ThreadPool;
+class ThreadTask;
 
 class Progress : public Dialog {
 public:
-  Progress(DownloadQueue *);
+  Progress(ThreadPool *);
 
 protected:
   void onInit() override;
@@ -35,11 +35,11 @@ protected:
   void onTimer(int) override;
 
 private:
-  void addDownload(Download *);
+  void addTask(ThreadTask *);
   void updateProgress();
 
-  DownloadQueue *m_queue;
-  auto_string m_currentName;
+  ThreadPool *m_pool;
+  auto_string m_current;
 
   HWND m_label;
   HWND m_progress;

src/thread.hpp
@@ -1,0 +1,144 @@
+/* ReaPack: Package manager for REAPER
+ * Copyright (C) 2015-2017  Christian Fillion
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef REAPACK_THREAD_HPP
+#define REAPACK_THREAD_HPP
+
+#include <array>
+#include <atomic>
+#include <functional>
+#include <queue>
+#include <unordered_set>
+
+#include <boost/signals2.hpp>
+#include <WDL/mutex.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <swell-types.h>
+#endif
+
+class ThreadTask {
+public:
+  enum State {
+    Idle,
+    Running,
+    Success,
+    Failure,
+    Aborted,
+  };
+
+  typedef boost::signals2::signal<void ()> VoidSignal;
+  typedef std::function<void ()> CleanupHandler;
+
+  ThreadTask();
+  virtual ~ThreadTask();
+
+  virtual std::string summary() const = 0;
+  virtual void run(void *) = 0;
+
+  void setState(State);
+  State state() const { return m_state; }
+
+  void onStart(const VoidSignal::slot_type &slot) { m_onStart.connect(slot); }
+  void onFinish(const VoidSignal::slot_type &slot) { m_onFinish.connect(slot); }
+  void setCleanupHandler(const CleanupHandler &cb) { m_cleanupHandler = cb; }
+
+  void abort() { m_abort = true; }
+
+  State m_state;
+  std::atomic_bool m_abort;
+
+  VoidSignal m_onStart;
+  VoidSignal m_onFinish;
+  CleanupHandler m_cleanupHandler;
+};
+
+class WorkerThread {
+public:
+  WorkerThread();
+  ~WorkerThread();
+
+  void push(ThreadTask *);
+  void clear();
+
+private:
+  static DWORD WINAPI run(void *);
+  ThreadTask *nextTask();
+
+  HANDLE m_wake;
+  HANDLE m_thread;
+  std::atomic_bool m_exit;
+  WDL_Mutex m_mutex;
+  std::queue<ThreadTask *> m_queue;
+};
+
+class ThreadPool {
+public:
+  typedef boost::signals2::signal<void ()> VoidSignal;
+  typedef boost::signals2::signal<void (ThreadTask *)> TaskSignal;
+
+  ThreadPool() {}
+  ThreadPool(const ThreadPool &) = delete;
+  ~ThreadPool();
+
+  void push(ThreadTask *);
+  void abort();
+
+  bool idle() const { return m_running.empty(); }
+
+  void onPush(const TaskSignal::slot_type &slot) { m_onPush.connect(slot); }
+  void onAbort(const VoidSignal::slot_type &slot) { m_onAbort.connect(slot); }
+  void onDone(const VoidSignal::slot_type &slot) { m_onDone.connect(slot); }
+
+private:
+  std::array<std::unique_ptr<WorkerThread>, 3> m_pool;
+  std::unordered_set<ThreadTask *> m_running;
+
+  TaskSignal m_onPush;
+  VoidSignal m_onAbort;
+  VoidSignal m_onDone;
+};
+
+// This singleton class receives state change notifications from a
+// worker thread and applies them in the main thread
+class ThreadNotifier {
+  typedef std::pair<ThreadTask *, ThreadTask::State> Notification;
+
+public:
+  static ThreadNotifier *get();
+
+  void start();
+  void stop();
+
+  void notify(const Notification &);
+
+private:
+  static ThreadNotifier *s_instance;
+  static void tick();
+
+  ThreadNotifier() : m_active(0) {}
+  ~ThreadNotifier() = default;
+  void processQueue();
+
+  WDL_Mutex m_mutex;
+  size_t m_active;
+  std::queue<Notification> m_queue;
+};
+
+#endif

src/about.cpp
@@ -18,6 +18,7 @@
 #include "about.hpp"
 
 #include "browser.hpp"
+#include "config.hpp"
 #include "encoding.hpp"
 #include "errors.hpp"
 #include "filesystem.hpp"

src/download.hpp
@@ -19,61 +19,33 @@
 #define REAPACK_DOWNLOAD_HPP
 
 #include "config.hpp"
+#include "thread.hpp"
 
-#include <array>
-#include <atomic>
-#include <functional>
-#include <queue>
 #include <string>
-#include <unordered_set>
 
-#include <boost/signals2.hpp>
-#include <WDL/mutex.h>
-
-#include <curl/curl.h>
-#include <reaper_plugin.h>
-
-class Download {
+class Download : public ThreadTask {
 public:
-  typedef boost::signals2::signal<void ()> VoidSignal;
-  typedef std::function<void ()> CleanupHandler;
-
-  enum State {
-    Idle,
-    Running,
-    Success,
-    Failure,
-    Aborted,
-  };
-
   enum Flag {
     NoCacheFlag = 1<<0,
   };
 
-  static void Init();
-  static void Cleanup();
+  static void GlobalInit();
+  static void GlobalCleanup();
+  static void *ContextInit();
+  static void ContextCleanup(void *);
 
   Download(const std::string &name, const std::string &url,
     const NetworkOpts &, int flags = 0);
-  ~Download();
 
-  const std::string &name() const { return m_name; }
+  std::string summary() const override;
   const std::string &url() const { return m_url; }
   const NetworkOpts &options() const { return m_opts; }
   bool has(Flag f) const { return (m_flags & f) != 0; }
 
-  void setState(State);
-  State state() const { return m_state; }
   const std::string &contents() { return m_contents; }
 
-  void onStart(const VoidSignal::slot_type &slot) { m_onStart.connect(slot); }
-  void onFinish(const VoidSignal::slot_type &slot) { m_onFinish.connect(slot); }
-  void setCleanupHandler(const CleanupHandler &cb) { m_cleanupHandler = cb; }
-
   void start();
-  void abort() { m_abort = true; }
-
-  void exec(CURL *);
+  void run(void *) override;
 
 private:
   static size_t WriteData(char *, size_t, size_t, void *);
@@ -83,86 +55,7 @@ private:
   std::string m_url;
   NetworkOpts m_opts;
   int m_flags;
-
-  State m_state;
-  std::atomic_bool m_abort;
   std::string m_contents;
-
-  VoidSignal m_onStart;
-  VoidSignal m_onFinish;
-  CleanupHandler m_cleanupHandler;
-};
-
-class DownloadThread {
-public:
-  DownloadThread();
-  ~DownloadThread();
-
-  void push(Download *);
-  void clear();
-
-private:
-  static DWORD WINAPI thread(void *);
-  Download *nextDownload();
-
-  HANDLE m_wake;
-  HANDLE m_thread;
-  std::atomic_bool m_exit;
-  WDL_Mutex m_mutex;
-  std::queue<Download *> m_queue;
-};
-
-class DownloadQueue {
-public:
-  typedef boost::signals2::signal<void ()> VoidSignal;
-  typedef boost::signals2::signal<void (Download *)> DownloadSignal;
-
-  DownloadQueue() {}
-  DownloadQueue(const DownloadQueue &) = delete;
-  ~DownloadQueue();
-
-  void push(Download *);
-  void abort();
-
-  bool idle() const { return m_running.empty(); }
-
-  void onPush(const DownloadSignal::slot_type &slot) { m_onPush.connect(slot); }
-  void onAbort(const VoidSignal::slot_type &slot) { m_onAbort.connect(slot); }
-  void onDone(const VoidSignal::slot_type &slot) { m_onDone.connect(slot); }
-
-private:
-  std::array<std::unique_ptr<DownloadThread>, 3> m_pool;
-  std::unordered_set<Download *> m_running;
-
-  DownloadSignal m_onPush;
-  VoidSignal m_onAbort;
-  VoidSignal m_onDone;
-};
-
-// This singleton class receives state change notifications from a
-// worker thread and applies them in the main thread
-class DownloadNotifier {
-  typedef std::pair<Download *, Download::State> Notification;
-
-public:
-  static DownloadNotifier *get();
-
-  void start();
-  void stop();
-
-  void notify(const Notification &);
-
-private:
-  static DownloadNotifier *s_instance;
-  static void tick();
-
-  DownloadNotifier() : m_active(0) {}
-  ~DownloadNotifier() = default;
-  void processQueue();
-
-  WDL_Mutex m_mutex;
-  size_t m_active;
-  std::queue<Notification> m_queue;
 };
 
 #endif

src/reapack.cpp
@@ -20,6 +20,7 @@
 #include "about.hpp"
 #include "browser.hpp"
 #include "config.hpp"
+#include "download.hpp"
 #include "errors.hpp"
 #include "filesystem.hpp"
 #include "index.hpp"
@@ -80,7 +81,7 @@ ReaPack::ReaPack(REAPER_PLUGIN_HINSTANCE instance)
   m_mainWindow = GetMainHwnd();
   m_useRootPath = new UseRootPath(resourcePath());
 
-  Download::Init();
+  Download::GlobalInit();
   RichEdit::Init();
 
   FS::mkdir(Path::CACHE);
@@ -105,7 +106,7 @@ ReaPack::~ReaPack()
   m_config->write();
   delete m_config;
 
-  Download::Cleanup();
+  Download::GlobalCleanup();
 
   delete m_useRootPath;
 }
@@ -287,12 +288,12 @@ void ReaPack::fetchIndexes(const vector<Remote> &remotes,
   // (in `parent`) to the progress dialog prevents it from being shown at all
   // while still taking the focus away from the manager dialog.
 
-  DownloadQueue *queue = new DownloadQueue;
-  Dialog *progress = Dialog::Create<Progress>(m_instance, m_mainWindow, queue);
+  ThreadPool *pool = new ThreadPool;
+  Dialog *progress = Dialog::Create<Progress>(m_instance, m_mainWindow, pool);
 
   auto load = [=] {
     Dialog::Destroy(progress);
-    delete queue;
+    delete pool;
 
     vector<IndexPtr> indexes;
 
@@ -309,16 +310,16 @@ void ReaPack::fetchIndexes(const vector<Remote> &remotes,
     callback(indexes);
   };
 
-  queue->onDone(load);
+  pool->onDone(load);
 
   for(const Remote &remote : remotes)
-    doFetchIndex(remote, queue, parent, stale);
+    doFetchIndex(remote, pool, parent, stale);
 
-  if(queue->idle())
+  if(pool->idle())
     load();
 }
 
-void ReaPack::doFetchIndex(const Remote &remote, DownloadQueue *queue,
+void ReaPack::doFetchIndex(const Remote &remote, ThreadPool *pool,
   HWND parent, const bool stale)
 {
   Download *dl = Index::fetch(remote, stale, m_config->network);
@@ -358,7 +359,7 @@ void ReaPack::doFetchIndex(const Remote &remote, DownloadQueue *queue,
     }
   });
 
-  queue->push(dl);
+  pool->push(dl);
 }
 
 IndexPtr ReaPack::loadIndex(const Remote &remote, HWND parent)
@@ -411,8 +412,7 @@ Transaction *ReaPack::setupTransaction()
   }
 
   assert(!m_progress);
-  m_progress = Dialog::Create<Progress>(m_instance, m_mainWindow,
-    m_tx->downloadQueue());
+  m_progress = Dialog::Create<Progress>(m_instance, m_mainWindow, m_tx->threadPool());
 
   m_tx->onFinish([=] {
     Dialog::Destroy(m_progress);

src/reapack.hpp
@@ -31,11 +31,11 @@
 class About;
 class Browser;
 class Config;
-class DownloadQueue;
 class Index;
 class Manager;
 class Progress;
 class Remote;
+class ThreadPool;
 class Transaction;
 
 typedef std::shared_ptr<const Index> IndexPtr;
@@ -89,7 +89,7 @@ public:
 
 private:
   void registerSelf();
-  void doFetchIndex(const Remote &remote, DownloadQueue *, HWND, bool stale);
+  void doFetchIndex(const Remote &remote, ThreadPool *, HWND, bool stale);
   IndexPtr loadIndex(const Remote &remote, HWND);
   void teardownTransaction();
 

commit 5aed825fda6c31518200805cbc6a8f14a331a98d

don't interpret background task's item names as format strings
Fixes a crash when downloading files containing '%', and a pretty big security vulnerability.

The bug was introduced in ecdc99394a260ea6fe802af996d6e9fd38a63168.

src/archive.cpp
@@ -221,7 +221,7 @@ int ArchiveReader::extractFile(const Path &path, std::ostream &stream) noexcept
 FileExtractor::FileExtractor(const Path &target, const ArchiveReaderPtr &reader)
   : m_path(target), m_reader(reader)
 {
-  setSummary("Extracting %s: " + target.join());
+  setSummary({ "Extracting", target.join() });
 }
 
 bool FileExtractor::run()
@@ -303,7 +303,7 @@ int ArchiveWriter::addFile(const Path &path, std::istream &stream) noexcept
 FileCompressor::FileCompressor(const Path &target, const ArchiveWriterPtr &writer)
   : m_path(target), m_writer(writer)
 {
-  setSummary("Compressing %s: " + target.join());
+  setSummary({ "Compressing", target.join() });
 }
 
 bool FileCompressor::run()

src/download.cpp
@@ -111,7 +111,7 @@ Download::Download(const std::string &url, const NetworkOpts &opts, const int fl
 
 void Download::setName(const std::string &name)
 {
-  setSummary("Downloading %s: " + name);
+  setSummary({ "Downloading", name });
 }
 
 bool Download::run()

src/progress.cpp
@@ -17,7 +17,6 @@
 
 #include "progress.hpp"
 
-#include "thread.hpp"
 #include "resource.hpp"
 #include "win32.hpp"
 
@@ -25,7 +24,7 @@
 
 Progress::Progress(ThreadPool *pool)
   : Dialog(IDD_PROGRESS_DIALOG),
-    m_pool(pool), m_label(nullptr), m_progress(nullptr),
+    m_pool(pool), m_current(), m_label(nullptr), m_progress(nullptr),
     m_done(0), m_total(0)
 {
   m_pool->onPush >> std::bind(&Progress::addTask, this, std::placeholders::_1);
@@ -68,7 +67,8 @@ void Progress::onTimer(const int id)
 void Progress::addTask(ThreadTask *task)
 {
   m_total++;
-  updateProgress();
+  if(m_current.step)
+    updateProgress();
 
   if(!isVisible())
     startTimer(100);
@@ -86,11 +86,11 @@ void Progress::addTask(ThreadTask *task)
 
 void Progress::updateProgress()
 {
-  Win32::setWindowText(m_label, String::format(m_current.c_str(),
-    String::format("%s of %s",
-      String::number(std::min(m_done + 1, m_total)).c_str(),
-      String::number(m_total).c_str()
-    ).c_str()
+  Win32::setWindowText(m_label, String::format("%s %s of %s: %s",
+    m_current.step,
+    String::number(std::min(m_done + 1, m_total)).c_str(),
+    String::number(m_total).c_str(),
+    m_current.item.c_str()
   ).c_str());
 
   const double pos = static_cast<double>(

src/progress.hpp
@@ -20,8 +20,7 @@
 
 #include "dialog.hpp"
 
-class ThreadPool;
-class ThreadTask;
+#include "thread.hpp"
 
 class Progress : public Dialog {
 public:
@@ -37,7 +36,7 @@ private:
   void updateProgress();
 
   ThreadPool *m_pool;
-  std::string m_current;
+  ThreadSummary m_current;
 
   HWND m_label;
   HWND m_progress;

src/thread.hpp
@@ -28,6 +28,11 @@
 #include <thread>
 #include <unordered_set>
 
+struct ThreadSummary {
+  const char *step;
+  std::string item;
+};
+
 class ThreadTask {
 public:
   enum State {
@@ -45,7 +50,7 @@ public:
   virtual bool concurrent() const = 0;
 
   void exec();  // runs in the current thread
-  const std::string &summary() const { return m_summary; }
+  const ThreadSummary &summary() const { return m_summary; }
   State state() const { return m_state; }
   void setError(const ErrorInfo &err) { m_error = err; }
   const ErrorInfo &error() { return m_error; }
@@ -59,10 +64,10 @@ public:
 protected:
   virtual bool run() = 0;
 
-  void setSummary(const std::string &s) { m_summary = s; }
+  void setSummary(const ThreadSummary &s) { m_summary = s; }
 
 private:
-  std::string m_summary;
+  ThreadSummary m_summary;
   State m_state;
   ErrorInfo m_error;
   std::atomic_bool m_abort;